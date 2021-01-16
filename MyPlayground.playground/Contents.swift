48124bafa59d2f9c8d24ac29ceab6318ec82a181
xtring <hh940630@gmail.com>
2021-01-17 02:02:51 +0900

    complete 'Playground 사용하기🌱'

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,7 +2,19 @@ import UIKit
 
 var str = "Hello, playground"
 
+/*
+ *  Playground 사용해보기
+ */
 
+// for in 사용
+// 0..<100 : 0에서 100까지
 for i in 0..<100 {
     i
 }
+
+// 0부터 100까지 돌면서 sin() 실행하기
+// sin() line의 우측 끝 눈양을 누르면 해당 코드가 어떤 식으로 실행되는지 확인 할 수 있다.
+for i in 0..<100 {
+    sin(Double(i) / 2)
+}
+
