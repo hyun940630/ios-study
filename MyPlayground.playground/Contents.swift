664c93f1f5046077def27d77ad06298526dad70b
xtring <hh940630@gmail.com>
2021-01-18 15:36:57 +0900

    learning 튜플(tuple)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -675,3 +675,63 @@ struct Hex2 {
 // 마찬가지로, willSet에서는 새로운 값을 newValue로 얻어올 수 있고, didSet에서는 예전 값을 oldValue라는 예약어를 통해 얻어올 수 있습니다.
 
 // willSet과 didSet은 일반적으로 어떤 속성의 값이 바뀌었을 대 UI를 업데이트하거나 특정 메서드를 호출하는 등의 역할을 할 때에 사용됩니다.
+
+
+
+
+
+// 튜플(Tuple)
+// 튜블(Tuple)은 어떠한 값들의 묶음입니다. 배열과 비슷하다고 볼 수 있습니다. 배열과 다르게 길이가 고정되어 있습니다. 값에 접근할 때에도 [] 대신 .을 사용합니다.
+var coffeeInfo = ("아메리카노", 5100)
+coffeeInfo.0    // 아메리카노
+coffeeInfo.1    // 5100
+coffeeInfo.1 = 5500
+
+// 이 튜플의 파라미터에 이름을 붙일 수도 있습니다.
+var namedCoffeeinfo = (coffee: "아메리카노", price: 5100)
+namedCoffeeinfo.coffee  // 아메리카노
+namedCoffeeinfo.price   // 5100
+namedCoffeeinfo.price = 5100
+
+// 앞서 본 구조체와 비슷합니다. 실제로도 간단한 자료형을 만들 때에는 구조체 대힌 튜플을 사용해서 만들기도 합니다.
+
+// 튜플의 타입 어노테이션은 아래와 같이 생겼습니다.
+var coffeeInfo2: (String, Int)
+var namedCoffeeInfo: (coffee: String, price: Int)
+
+// 튜플을 조금 응용하면, 아래와 같이 여러 변수에 값을 지정할 수도 있습니다.
+let (coffee, price) = ("아메리카노", 5100)
+coffee
+price
+
+// 튜플이 가진 값을 가지고 변수에 값을 지정할 때, 무시하고 싶은 값이 있다면 _ 키워드를 사용해서 할 수 있습니다. 아래 코드에서는 "라떼라는 첫 번재 값을 무시합니다."
+let (_, latteSize, lattePrice) = ("라떼", "Venti", 5600)
+latteSize   // Venti
+lattePrice  // 5600
+
+// 물론, 튜플을 반환하는 함수도 만들 수 있습니다.
+// 커피 이름에 맞는 커피 가격 정보를 반환합니다. 일치하는 커피 이름이 없다면 'nil'을 반환합니다.
+//
+// - parameter name : 커피 이름
+//
+// - returns : 커피 이름과 가격 정보로 구성된 튜플을 반환합니다.
+func coffeeInfo3(for name: String) -> (name: String, price: Int)? {
+    let coffeeInfoList: [(name: String, price: Int)] = [
+        ("아메리카노", 5100),
+        ("라떼", 5600)
+    ]
+    for coffeeInfo in coffeeInfoList {
+        if coffeeInfo.name == name {
+            return coffeeInfo
+        }
+    }
+    return nil
+}
+
+
+coffeeInfo3(for: "아메리카노")?.price    // 5100
+coffeeInfo3(for: "에스프레소")?.price    // nil
+
+let (_, lattePrice2) = coffeeInfo3(for: "라떼")!
+lattePrice2     // 5600
+
