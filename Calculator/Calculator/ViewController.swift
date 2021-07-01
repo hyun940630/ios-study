8098e2346c584d6d2681c5015cddb49bc49b711e
xtring <hh940630@gmail.com>
2021-07-01 18:00:33 +0900

    init

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by xtring on 2021/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Hello world")
+    }
+
+
+}
+
