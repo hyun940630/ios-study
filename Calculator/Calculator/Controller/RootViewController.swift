d8224b9daaa3eac750fe95ee90370307b86ea8f8
xtring <hh940630@gmail.com>
2021-07-01 18:27:08 +0900

    Make: UIButton, configureUI() -공통으로 사용할 버튼을 생성 -configureUI에서 Button에 대한 위치 속성을 잡아줌

--- Calculator/Calculator/Controller/RootViewController.swift
+++ Calculator/Calculator/Controller/RootViewController.swift
@@ -12,13 +12,13 @@ class RootViewController:UIViewController {
     // MARK - Properties
     
     // 버튼을 만든다! 버튼은 속성값이므로 여기서!
+    // lazy 키워드를 사용하면 '가능한 늦게 만들어!'라는 의미로 나중에 여러개 생성될 버튼들이 각각의 기능을 할 수 있도록 함. 속성을 동적으로 만듬
     private lazy var numberButton:UIButton = {
-        let bt = UIButton(type: UIButton.ButtonType.system)
-        
-        var varkeyword = "var"  // 값의 변경이 가능
-        let letkeyowrd = "let"  // 값을 변경할 수 없음. constant value
+        let bt = UIButton(type: UIButton.ButtonType.system)     // 대체적(일반적)으로 color값은 system, normal을 많이 사용한다
+        bt.setTitle("1", for: UIControl.State.normal)
         return bt
     }()
+    // ()의 의미: 실행한다.
     
     // MARK: - Lifecycles
     
@@ -35,5 +35,10 @@ class RootViewController:UIViewController {
     func configureUI() {
         // 처음에 설정하지 않으면 실제로는 '검정'이며 이는 실제로 색이 입혀진 것이 아니다.
         view.backgroundColor = .systemBackground    // system의 color 세팅을 따라 설정된다.
+    
+        view.addSubview(numberButton)
+        numberButton.translatesAutoresizingMaskIntoConstraints = false      // 꼭 넣어줘라. 코드로 UI를 생성했을 때 UI가 동적으로 기능을 가질 수 있다.
+        numberButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        numberButton.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
     }
 }
